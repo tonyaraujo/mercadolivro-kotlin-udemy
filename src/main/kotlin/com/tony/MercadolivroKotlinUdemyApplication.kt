52dec508afc57c42850c78372761b9fd71fc4787
Tony Araújo <tnr.arauo@gmail.com>
2022-01-05 18:27:33 -0300

    first commit

---
+++ src/main/kotlin/com/tony/MercadolivroKotlinUdemyApplication.kt
@@ -0,0 +1,11 @@
+package com.tony
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MercadolivroKotlinUdemyApplication
+
+fun main(args: Array<String>) {
+	runApplication<MercadolivroKotlinUdemyApplication>(*args)
+}
---
+++ src/test/kotlin/com/tony/MercadolivroKotlinUdemyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tony
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MercadolivroKotlinUdemyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
